7e88d6c2edf6d1db6cd0ff507b7a9c2f935f0007
Liangliang Zhu <shinyypig@gmail.com>
2023-03-05 00:08:43 +0800

    🩹 fix pca project direction error

--- main.m
+++ main.m
@@ -12,7 +12,7 @@ him = double(him) / 4095;
 X = reshape(him, [], l);
 % perform PCA
 [P, ~, ~] = svds(X' * X, k);
-X_pca = X * P * P(1:k, :)';
+X_pca = X * P(:, 1:k);
 him_pca = reshape(X_pca, m, n, k);
 
 % show the results
